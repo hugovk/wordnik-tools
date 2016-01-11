791006fff9f7fe26843f0e7ca159b7846eea52c8
hugovk <hugovk@users.noreply.github.com>
2016-01-11 14:06:32 +0200

    Add --list to get comments from all words in a list

--- wordnik_comment_scraper.py
+++ wordnik_comment_scraper.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-Download comments (from a user) on a word.
+Download comments (from a user) on a word (or list thereof).
 Scrapes because no comment fetching via API.
 """
 from __future__ import print_function
 import argparse
 import urllib2
-from urlparse import urljoin  # Python 2
+import sys
 from bs4 import BeautifulSoup, Comment  # pip install BeautifulSoup4
+from urlparse import urljoin  # Python 2
+from urllib import quote
 
 
 def fix_relative_links(soup, base_url):
@@ -22,14 +24,14 @@ def fix_relative_links(soup, base_url):
 
 def scrape_word_comments(slug, user=None):
     # """Scrape a Wordnik word and return a list of comments"""
-    url = "https://wordnik.com/words/" + slug
+    found = []
+
+    url = "https://wordnik.com/words/" + quote(slug.encode('utf8'), safe="")
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page.read(), "lxml")
 
     ul_comments = soup.find(id="commentsOnWord")
     li_comments = ul_comments.find_all("li", class_="comment")
-
-    found = []
 
     for comment in li_comments:
         if user:
@@ -56,24 +58,44 @@ def scrape_word_comments(slug, user=None):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Download a Wordnik list to a text file.",
+        description="Download comments (from a user) on a word (or list).",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        'slug',
+        '-w', '--word',
         help="Word slug, eg. spinning%%20rust for "
-             "https://www.wordnik.com/words/spinning%%20rust")
+             "https://wordnik.com/words/spinning%%20rust")
+    parser.add_argument(
+        '-l', '--list',
+        help="List slug, eg. new-to-me--2015 for "
+             "https://wordnik.com/lists/new-to-me--2015")
     parser.add_argument(
         '-u', '--user',
-        help="Restrict to this user")
+        help="Restrict to this user",
+        default="hugovk")
     parser.add_argument(
         '-o', '--outfile',
         help="Save to this file. Default: <slug>.txt")
     args = parser.parse_args()
 
-    comments = scrape_word_comments(args.slug, args.user)
+    if args.word and args.list:
+        sys.exit("Please give just a word or list, not both")
+    elif not args.word and not args.list:
+        sys.exit("Please give a word or list")
 
-    for comment in comments:
-        print(comment)
+    if args.word:
+        words = [args.word]
+
+    if args.list:
+        from wordnik_list_scraper import scrape_list, sort_words
+        words = scrape_list(args.list)
+        words = sort_words(words)
+
+    comments = []
+    for word in words:
+        new_comments = scrape_word_comments(word, args.user)
+        comments.extend(new_comments)
+        for comment in new_comments:
+            print(comment)
 
     # word_string = '\n'.join(words)
     # print(word_string)
